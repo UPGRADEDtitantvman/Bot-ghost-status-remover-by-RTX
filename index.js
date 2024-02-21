b45f3c74d2ce5e9352096f41e3067e8d76ae319f
UPGRADEDtitantvman <156999100+UPGRADEDtitantvman@users.noreply.github.com>
2024-02-21 20:49:19 +0700

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["VietJet","VietNam Airline", "Bamboo Airway", "Asia"];
 
 
 let currentIndex = 0;
